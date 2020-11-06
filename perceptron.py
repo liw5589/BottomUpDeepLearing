02e2620b561f81e3fa934a7d63bae15a19ef3cf5
liw5589 <liw5589@naver.com>
2020-11-06 10:07:03 +0900

    ADD XOR function

--- perceptron.py
+++ perceptron.py
@@ -67,4 +67,16 @@ print("OR GATE's Result : ",OR_Gate(0,0))
 print("OR GATE's Result : ",OR_Gate(0,1))
 print("OR GATE's Result : ",OR_Gate(1,0))
 print("OR GATE's Result : ",OR_Gate(1,1))
+print("")
 
+def XOR_Gate(x1,x2):
+    s1 = NAND_Gate(x1,x2)
+    s2 = OR_Gate(x1,x2)
+    y = AND_Gate_using_np(s1,s2)
+    return y
+    
+print("*** XOR GATE RESULT Using np ***")
+print("XOR GATE's Result : ",XOR_Gate(0,0))
+print("XOR GATE's Result : ",XOR_Gate(0,1))
+print("XOR GATE's Result : ",XOR_Gate(1,0))
+print("XOR GATE's Result : ",XOR_Gate(1,1))
