d6c44b793191ee2833af10f3a88e7aacbd144a49
liw5589 <liw5589@naver.com>
2020-11-09 17:31:32 +0900

    ADD numpy neuralNetwork

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,46 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def step_function(x):
-    y = x > 0
-    return y.astype(np.int)
-    
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-x = np.arange(-5,5,0.1)
-y = step_function(x)
+def identify_function(x):
+    return x
 
-plt.plot(x,y,color = "blue", alpha = 0.5)
-plt.title("Step Function Plot", fontsize = 20)
-plt.ylim(-0.1,1.1)
-plt.savefig("Img/StepPlot.png")
-plt.show()
+X = np.array([1.0,0.5])
+W1 = np.array([[0.1,0.3,0.5],[0.2,0.4,0.6]])
+B1 = np.array([0.1,0.2,0.3])
 
-x = np.arange(-5,5,0.1)
-y = sigmoid(x)
+A1 = np.dot(X,W1) + B1
+Z1 = sigmoid(A1)
 
-plt.plot(x,y,color = "red", alpha = 0.5)
-plt.title("Sigmoid Plot", fontsize = 20)
-plt.ylim(-0.1,1.1)
-plt.savefig("Img/SigmoidPlot.png")
-plt.show()
+W2 = np.array([[0.1,0.4],[0.2,0.5],[0.3,0.6]])
+B2 = np.array([0.1,0.2])
 
-y = sigmoid(x)
-y1 = step_function(x)
-plt.plot(x,y1,color = "blue", alpha = 0.5)
-plt.plot(x,y,color = "red", alpha = 0.5)
-plt.title("Step & Sigmoid Plot", fontsize = 20)
-plt.ylim(-0.1,1.1)
-plt.savefig("Img/Both.png")
-plt.show()
+A2 = np.dot(Z1, W2) + B2
+Z2 = sigmoid(A2)
 
-def relu(x):
-    return np.maximum(0,x)
+W3 = np.array([[0.1,0.3],[0.2,0.4]])
+B3 = np.array([0.1,0.2])
 
-y = relu(x)
-plt.plot(x,y,color = "red", alpha = 0.5)
-plt.title("ReLU Plot", fontsize = 20)
-plt.ylim(-0.1,5.1)
-plt.savefig("Img/ReLU Plot.png")
-plt.show()
+A3 = np.dot(Z2, W3) + B3
+Y = identify_function(A3)
+
+print(Y)
